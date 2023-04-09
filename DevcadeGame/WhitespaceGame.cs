f197b5c335228492574af3c342d05a33a52f5cf2
Jonathan Jan (Laptop) <jmj2097@rit.edu>
2023-04-08 22:06:06 -0400

    created initial setup changed

--- DevcadeGame/WhitespaceGame.cs
+++ DevcadeGame/WhitespaceGame.cs
@@ -3,39 +3,26 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Devcade;
 
-// MAKE SURE YOU RENAME ALL PROJECT FILES FROM DevcadeGame TO YOUR YOUR GAME NAME
-namespace DevcadeGame
+namespace Whitespace
 {
-	public class Game1 : Game
+	public class WhitespaceGame : Game
 	{
 		private GraphicsDeviceManager _graphics;
 		private SpriteBatch _spriteBatch;
 		
-		/// <summary>
-		/// Stores the window dimensions in a rectangle object for easy use
-		/// </summary>
-		private Rectangle windowSize;
 		
-		/// <summary>
-		/// Game constructor
-		/// </summary>
-		public Game1()
+		public WhitespaceGame()
 		{
 			_graphics = new GraphicsDeviceManager(this);
 			Content.RootDirectory = "Content";
 			IsMouseVisible = false;
 		}
 
-		/// <summary>
-		/// Performs any setup that doesn't require loaded content before the first frame.
-		/// </summary>
 		protected override void Initialize()
 		{
 			// Sets up the input library
 			Input.Initialize();
-
-			// Set window size if running debug (in release it will be fullscreen)
-			#region
+			#region Set to Devcade Resolution
 #if DEBUG
 			_graphics.PreferredBackBufferWidth = 420;
 			_graphics.PreferredBackBufferHeight = 980;
@@ -46,11 +33,10 @@ namespace DevcadeGame
 			_graphics.ApplyChanges();
 #endif
 			#endregion
+			////
 			
-			// TODO: Add your initialization logic here
 
-			windowSize = GraphicsDevice.Viewport.Bounds;
-			
+			////
 			base.Initialize();
 		}
 
@@ -60,32 +46,27 @@ namespace DevcadeGame
 		protected override void LoadContent()
 		{
 			_spriteBatch = new SpriteBatch(GraphicsDevice);
-
-			// TODO: use this.Content to load your game content here
-			// ex:
-			// texture = Content.Load<Texture2D>("fileNameWithoutExtension");
+			////
+			
+			
 		}
 
-		/// <summary>
-		/// Your main update loop. This runs once every frame, over and over.
-		/// </summary>
-		/// <param name="gameTime">This is the gameTime object you can use to get the time since last frame.</param>
 		protected override void Update(GameTime gameTime)
 		{
 			Input.Update(); // Updates the state of the input library
-
-			// Exit when both menu buttons are pressed (or escape for keyboard debugging)
-			// You can change this but it is suggested to keep the keybind of both menu
-			// buttons at once for a graceful exit.
+			//Emergency Exit
 			if (Keyboard.GetState().IsKeyDown(Keys.Escape) ||
 				(Input.GetButton(1, Input.ArcadeButtons.Menu) &&
 				Input.GetButton(2, Input.ArcadeButtons.Menu)))
 			{
 				Exit();
 			}
+			////
+			
 
-			// TODO: Add your update logic here
 
+
+			////
 			base.Update(gameTime);
 		}
 
@@ -95,11 +76,9 @@ namespace DevcadeGame
 		/// <param name="gameTime">This is the gameTime object you can use to get the time since last frame.</param>
 		protected override void Draw(GameTime gameTime)
 		{
-			GraphicsDevice.Clear(Color.CornflowerBlue);
-			
-			// Batches all the draw calls for this frame, and then performs them all at once
+			GraphicsDevice.Clear(Color.Black);
 			_spriteBatch.Begin();
-			// TODO: Add your drawing code here
+			
 			
 			_spriteBatch.End();
 
