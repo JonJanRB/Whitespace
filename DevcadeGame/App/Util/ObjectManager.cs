2b13f8babdb17745de2b4591dca1681d96e16887
Jonathan Jan (Laptop) <jmj2097@rit.edu>
2023-04-13 10:23:28 -0400

    removed shape batch and created object manager

---
+++ DevcadeGame/App/Util/ObjectManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whitespace.App.Util
+{
+    /// <summary>
+    /// Singelton that has orbs and spikes
+    /// </summary>
+    internal class ObjectManager
+    {
+    }
+}
